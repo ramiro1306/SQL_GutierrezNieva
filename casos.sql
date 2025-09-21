90d52d49e551c1400cec90d5cc2ac6547e44ce5d
ramiro1306 <101232805+ramiro1306@users.noreply.github.com>
2025-09-21 11:36:24 -0300

    .

--- casos.sql
+++ casos.sql
@@ -1,2 +1,13 @@
 USE cafeteria;
 
+-- Consultas: Obtener la cantidad de pedidos del dni ingresado:
+SELECT cafeteria.contar_pedidos_cliente(12345678) AS pedidos_cliente;
+
+-- Consultas: Obtener el producto mas vendido:
+SELECT cafeteria.producto_mas_vendido();
+
+-- Consultas: Obtener el pedido de un cliente:
+CALL listar_pedidos_cliente(12345678);
+
+-- Consultas: Agregar productos:
+CALL cafeteria.agregar_producto('Lemon pie', 2, 1200, 15);
